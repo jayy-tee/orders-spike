553714338aa8c330902c9d9195f7d4650f6e3ece
John Taylor <john.taylor@servcorp.com.au>
2020-07-18 18:33:54 +1000

    feat: Add Health Controller (k8s readiness check)

---
+++ src/Acme.Orders.Api/Controllers/HealthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.Orders.Application.Exceptions;
+using Acme.Orders.Application.Model;
+using Acme.Orders.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Acme.Orders.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        private readonly ILogger<HealthController> _logger;
+        private readonly IMediator _mediator;
+
+        public HealthController(ILogger<HealthController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        [Route("ready")]
+        public async Task<IActionResult> CheckReadyForTraffic()
+        {
+            try
+            {
+                var order = await _mediator.Send(new GetOrderQuery(Guid.NewGuid()));
+            }
+            catch (NotFoundException)
+            {
+                // Swallow the exception for negative test case.
+            }
+            
+            return Ok();
+        }
+    }
+}
