80fdd9bc305cf9df92f3293c0cf37ae956270fad
John Taylor <49388196+jayy-tee@users.noreply.github.com>
2020-07-19 17:56:05 +1000

    chore: Update settings

--- src/Acme.Orders.Api/Config/ConfigurationSingleton.cs
+++ src/Acme.Orders.Api/Config/ConfigurationSingleton.cs
@@ -34,8 +34,9 @@ namespace Acme.Orders.Api.Config
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile("serilogSettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true)
-            .AddJsonFile($"serilogSettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true);
-
+            .AddJsonFile($"serilogSettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true)
+            .AddEnvironmentVariables();
+            
             return result;
         }            
     }
